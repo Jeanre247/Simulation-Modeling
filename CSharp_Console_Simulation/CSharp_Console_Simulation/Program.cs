f2b83ecfacb2496a2bcbb38e8bdf5cad251c19d0
Jeanre <jeanre247@gmail.com>
2017-11-15 14:07:19 +0200

    C# + VB console solutions.
    This is the solution to the simulation question. Bothe these solutions are console apps. There will still follow a java console solution and a C# windows form solution.

---
+++ CSharp_Console_Simulation/CSharp_Console_Simulation/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Console_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int profit = 8 - 4;
+            double avg = 0.0;
+
+            for (int s = 0; s < 1000; s++)
+            {
+                avg += simulateMonth() * (profit);
+            }// end of for
+
+            avg /= 1000;
+            Console.WriteLine(avg);
+            Console.ReadLine();
+        }
+
+        public static int simulateMonth()
+        {
+            Random random = new Random();
+            int lostClients = 0;
+            int reorderPoint = 25;
+            int orderSize = 35;
+            int stock = 0;
+            int receiveDay = -1;
+            bool activeOrder = false;
+
+            for (int i = 1; i <= 28; i++)
+            {
+
+                // subtract sales...
+                stock -= getRandomDemand(random);
+                //System.out.println(stock);
+
+                // stock received
+                if (i == receiveDay)
+                {
+                    stock += orderSize;
+                    activeOrder = false;
+                    receiveDay = -1;
+                }// end if
+
+                // lost sales or clients
+                if (stock <= 0)
+                {
+                    lostClients += Math.Abs(stock);
+                    stock = 0;
+                }// end if
+
+                // reorder stock at reorderPoint of 25
+                if (stock <= reorderPoint && !activeOrder)
+                {
+                    receiveDay = getRandomLeadTime(random) + i;
+                    activeOrder = true;
+                }// end if
+
+            }// end of for
+
+            return lostClients;
+        }// end of simulateMonth
+
+        public static int getRandomDemand(Random r)
+        {
+            double num = r.NextDouble();
+
+            if (num < 0.261306533)
+            {
+                return 5;
+            }
+            else if (num < 0.48241206)
+            {
+                return 6;
+            }
+            else if (num < 0.804020101)
+            {
+                return 7;
+            }
+            else
+            {
+                return 8;
+            }// end if else
+        }// end of getRandomDemand
+
+        public static int getRandomLeadTime(Random r)
+        {
+            double num = r.NextDouble();
+
+            if (num < 0.285714286)
+            {
+                return 2;
+            }
+            else if (num < 0.642857143)
+            {
+                return 3;
+            }
+            else
+            {
+                return 4;
+            }// end if else
+        }// end of getRandomLeadTime
+    }// end of class
+}// end of simulation.
---
+++ VB_Console_Simulation/VB_Console_Simulation/Module1.vb
@@ -0,0 +1,85 @@
+﻿Module Module1
+
+    Public reorderPoint As Integer = 25
+
+    Public orderSize As Integer = 35
+
+    Sub Main()
+        Randomize()
+
+
+        Dim profit As Integer = (8 - 4)
+        Dim avg As Double = 0
+        Dim s As Integer = 0
+        Do While (s < 1000)
+            avg += simulateMonth() * profit
+
+            s = (s + 1)
+        Loop
+
+        avg = (avg / 1000)
+        Console.WriteLine("Average profit lost for January 2018:")
+        Console.WriteLine(avg)
+        Console.WriteLine("Simulation was run 1000 times.")
+        Console.ReadLine()
+
+    End Sub
+
+    Public Function simulateMonth() As Integer
+        Dim receiveDay As Integer = -1
+        Dim stock As Integer = 0
+        Dim lostClients As Integer = 0
+        Dim hasActiveOrder As Boolean = False
+        Dim i As Integer = 1
+        Do While (i <= 28)
+            Dim demand As Integer = getRandomDayDemand()
+            stock = (stock - demand)
+            If (i = receiveDay) Then
+                receiveDay = -1
+                stock = (stock + orderSize)
+                hasActiveOrder = False
+            End If
+
+            If (stock < 0) Then
+                lostClients = (lostClients + Math.Abs(stock))
+                stock = 0
+            End If
+
+            If ((stock <= reorderPoint) And (Not hasActiveOrder)) Then
+                receiveDay = (getRandomLeadTime() + i)
+                hasActiveOrder = True
+            End If
+
+            i = (i + 1)
+        Loop
+
+        Return lostClients
+    End Function
+
+    Private Function getRandomLeadTime() As Integer
+        Dim r = Rnd()
+        If (r < 0.285714286) Then
+            Return 2
+        ElseIf (r < 0.642857143) Then
+            Return 3
+        Else
+            Return 4
+        End If
+
+    End Function
+
+    Private Function getRandomDayDemand() As Integer
+        Dim r = Rnd()
+        If (r < 0.261306533) Then
+            Return 5
+        ElseIf (r < 0.48241206) Then
+            Return 6
+        ElseIf (r < 0.804020101) Then
+            Return 7
+        Else
+            Return 8
+        End If
+
+    End Function
+
+End Module
